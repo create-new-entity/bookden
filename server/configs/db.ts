4ff5608b6db983a32a320fce4e68fcde3d1d38b7
Pavel <imran.pavel@gmail.com>
2025-10-24 17:57:49 +0300

    Set up test db for testing

--- server/configs/db.ts
+++ server/configs/db.ts
@@ -18,6 +18,13 @@ const poolOptions: PoolOptions = {
 
 let pgDBPool: DatabasePool | undefined;
 
+const DB_URL = (() => {
+    if (process.env.NODE_ENV === 'test') {
+        return process.env.LOCAL_TEST_DB_URL;
+    }
+    return process.env.LOCAL_DB_URL;
+})();
+
 const getPGDBPool = () => {
     return pgDBPool;
 };
@@ -36,8 +43,8 @@ const sqlFragment = slonikSql.fragment;
 
 const initPGDBPool = async () => {
     try {
-        console.log(`DB Connection URL: ${process.env.LOCAL_DB_URL}`);
-        pgDBPool = await createPool(process.env.LOCAL_DB_URL + '', poolOptions);
+        console.log(`NODE_ENV: ${process.env.NODE_ENV}\nDB Connection URL: ${DB_URL}`);
+        pgDBPool = await createPool(DB_URL + '', poolOptions);
         console.log('PostgreSQL connection pool initialized.');
     } catch (error) {
         console.error('Error initializing PostgreSQL connection pool:', error);
