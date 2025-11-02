7e73890e75657497aa91a03e4cde74d7e8949e9d
Pavel <imran.pavel@gmail.com>
2025-11-02 17:53:47 +0200

    basic navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import LogInPage from './pages/LogInPage';
 import HomePage from './pages/HomePage';
+import NavBar from './components/NavBar';
 
 const App = () => {
     return (
-        <Routes>
-            <Route path="/auth" element={<LogInPage/>} />
-            <Route path="/" element={<HomePage/>} />
-        </Routes>
+        <>
+            <Routes>
+                <Route path="/auth" element={<LogInPage/>} />
+                <Route path="/" element={<HomePage/>} />
+            </Routes>
+            <NavBar/>
+        </>
     );
 };
 
---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,74 @@
+import { AppBar, Autocomplete, Avatar, Box, Button, Menu, MenuItem, Stack, TextField, Toolbar, useTheme } from '@mui/material';
+import { useState } from 'react';
+
+
+const styles: Record<string, React.CSSProperties> = {
+    rootStack: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    searchAndAvatarContainerStack: {
+        width: '66%',
+        height: '100%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flex: 1
+    },
+    searchBox: {
+        position: 'absolute',
+        left: '50%',
+        transform: 'translateX(-50%)'
+    },
+    bookdenLogo: {
+        height: '2.5rem'
+    }
+};
+
+const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [anchorElement, setAnchorElement] = useState<null | HTMLDivElement>(null);
+    const theme = useTheme();
+
+    return (
+        <AppBar>
+            <Toolbar>
+                <Stack direction={'row'} sx={styles.rootStack}>
+                    <Button onClick={() => console.log('Clicked home logo')}>
+                        <Box sx={{ ...styles.bookdenLogo, color: 'yellow' }}>
+                            <img
+                                src="/assets/book-den.svg"
+                                alt="Book Den Logo"
+                                style={{ width: '100%', height: '100%', objectFit: 'contain', color: 'purple' }}
+                            />
+                        </Box>
+                    </Button>
+                    <Autocomplete
+                        sx={styles.searchBox}
+                        options={[]}
+                        renderInput={() => <TextField/>}
+                    />
+                    <Avatar onClick={(e: React.MouseEvent<HTMLDivElement>) => {
+                        setAnchorElement(e.currentTarget);
+                        setMenuOpen(true);
+                    }}/>
+                    <Menu
+                        anchorEl={anchorElement}
+                        open={menuOpen}
+                        onClose={() => {
+                            setMenuOpen(false);
+                            setAnchorElement(null);
+                        }}
+                    >
+                        <MenuItem onClick={() => console.log('Clicked Profile')}>Profile</MenuItem>
+                        <MenuItem onClick={() => console.log('Clicked My account')}>My account</MenuItem>
+                        <MenuItem onClick={() => console.log('Clicked Logout')}>Logout</MenuItem>
+                    </Menu>
+                </Stack>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default NavBar;
