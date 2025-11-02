13ea608eca689079777959b6300905c428dd5f31
Pavel <imran.pavel@gmail.com>
2025-11-02 18:17:42 +0200

    Update HomePage.tsx

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -14,7 +14,7 @@ const HomePage = () => {
     }, [navigate, isLoggedIn]);
 
     return (
-        <div style={{ marginTop: 300 }}>
+        <div style={{ paddingTop: 200 }}>
             Hello there and welcome!
             <button onClick={handleLoggedOutContext}>Log Out</button>
         </div>
