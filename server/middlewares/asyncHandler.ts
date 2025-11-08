cc9a5475128416bf4b1efab6767c2c28be33e1ef
Pavel <imran.pavel@gmail.com>
2025-11-08 14:51:04 +0200

    asyncHandler to reduce try catch boilerplate

---
+++ server/middlewares/asyncHandler.ts
@@ -0,0 +1,20 @@
+
+
+
+import { Request, Response, NextFunction } from 'express';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type HandlerFn = (req: Request, res: Response, next: NextFunction) => Promise<any>;
+
+
+/*
+    Puropse of this middleware is to wrap the async routes in a
+    try-catch block and pass the error to the next middleware.
+
+    This is to avoid the repetitive try-catch blocks in the routes.
+ */
+export const asyncHandler = (fn: HandlerFn) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        Promise.resolve(fn(req, res, next)).catch(next);
+    };
+};
--- server/routes/loginRoutes.ts
+++ server/routes/loginRoutes.ts
@@ -1,19 +1,15 @@
 import { Router } from 'express';
 import login from '../controllers/login';
+import { asyncHandler } from '../middlewares/asyncHandler';
 
 export const loginBaseUrl = '/api/login';
 
 const loginRouter = Router();
 
-loginRouter.post('/', async (req, res, next) => {
-    try {
-        const { username, password } = req.body;
-        const result = await login(username, password);
-        res.status(200).json(result);
-    }
-    catch (error) {
-        next(error);
-    }
-});
+loginRouter.post('/', asyncHandler(async (req, res, _next) => {
+    const { username, password } = req.body;
+    const result = await login(username, password);
+    res.status(200).json(result);
+}));
 
 export default loginRouter;
