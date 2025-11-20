8ac9e51d470348446c08a52cdc9525bd659a4b28
Pavel <imran.pavel@gmail.com>
2025-11-20 22:24:28 +0200

    refactored imports

--- server/app.ts
+++ server/app.ts
@@ -2,12 +2,18 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 
-import loginRouter, { loginBaseUrl } from './routes/loginRoutes';
-import userRouter, { userBaseUrl } from './routes/userRoutes';
-import { errorHandler } from './middlewares/errorhandler';
-import testRouter, { testBaseUrl } from './routes/testRoutes';
-import avatarRouter, { avatarBaseUrl } from './routes/avatarRoutes';
 import { isTestEnvironment } from './configs';
+import {
+    loginBaseUrl,
+    loginRouter,
+    userBaseUrl,
+    userRouter,
+    testBaseUrl,
+    testRouter,
+    avatarBaseUrl,
+    avatarRouter,
+} from './routes';
+import { errorHandler } from './middlewares';
 
 const app = express();
 
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,4 +1,11 @@
 export * from './loginRoutes';
+export { default as loginRouter } from './loginRoutes';
+
 export * from './userRoutes';
+export { default as userRouter } from './userRoutes';
+
 export * from './avatarRoutes';
+export { default as avatarRouter } from './avatarRoutes';
+
 export * from './testRoutes';
+export { default as testRouter } from './testRoutes';
