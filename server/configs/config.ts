8a4a46ee3d35fabde95635745d8bfacbd25a3a04
Pavel <imran.pavel@gmail.com>
2025-11-26 03:33:38 +0200

    login payload validation and env check

--- server/configs/config.ts
+++ server/configs/config.ts
@@ -1,8 +1,15 @@
 import 'dotenv/config';
+import { errorMessages, errorNames } from '../errors';
 
 const PORT = process.env.PORT;
 const JWT_SECRET = process.env.JWT_SECRET;
 const NODE_ENV = process.env.NODE_ENV;
+
+if(!PORT || !JWT_SECRET || !NODE_ENV) {
+    throw new Error(`${errorMessages[errorNames.envVarUndefined]}: ${PORT ? '' : 'PORT'} ${JWT_SECRET ? '' : 'JWT_SECRET'} ${NODE_ENV ? '' : 'NODE_ENV'}`);
+}
+
+
 export const ENV_VARIABLES = {
     PORT, JWT_SECRET, NODE_ENV
 };
--- server/validation/user.ts
+++ server/validation/user.ts
@@ -50,3 +50,8 @@ export const GetUsersQueryParamsSchema = z.object({
     page: z.string().optional(),
 });
 
+
+export const LoginSchema = z.object({
+    username: z.string().min(6).max(30),
+    password: z.string().min(6).max(30),
+});
