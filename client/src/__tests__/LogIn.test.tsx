abe083fee09b08025031e554b59b07913afc7402
Pavel <imran.pavel@gmail.com>
2026-01-22 21:08:36 +0200

    Signup login integration tests

--- client/src/__tests__/LogIn.test.tsx
+++ client/src/__tests__/LogIn.test.tsx
@@ -1,22 +1,56 @@
 
-
-import { render, screen } from '@testing-library/react';
-import LogIn from '../pages/LogInPage';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { render, screen, waitFor } from '@testing-library/react';
+import { vi } from 'vitest';
+
+const mockLogin = vi.fn();
+const mockSignUp = vi.fn();
+
+vi.mock('../api', () => ({
+    signUp: (payload: unknown) => mockSignUp(payload),
+    login: (payload: unknown) => mockLogin(payload),
+}));
+
+
+import LogInPage from '../pages/LogInPage';
+import userEvent from '@testing-library/user-event';
+
 
 const queryClient = new QueryClient();
 
-test('Renders login form fields', () => {
+const renderLogInComponent = () => {
     render(
         <QueryClientProvider client={queryClient}>
             <BrowserRouter>
-                <LogIn />
+                <LogInPage />
             </BrowserRouter>
         </QueryClientProvider>
     );
+};
 
-    expect(screen.getByPlaceholderText(/username/i)).toBeInTheDocument();
-    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /log in/i })).toBeInTheDocument();
+describe('Login flow', () => {
+    test('Login form renders and sends valid credentials to API function', async () => {
+        const user = userEvent.setup();
+        renderLogInComponent();
+    
+        await user.click(
+            screen.getByRole('tab', { name: /log in/i })
+        );
+    
+        await user.type(screen.getByLabelText(/username/i), 'customer0');
+        await user.type(screen.getByLabelText(/password/i), 'verystrongpasword0');
+    
+        await user.click(
+            screen.getByRole('button', { name: /log in/i })
+        );
+    
+        await waitFor(() => {
+            expect(mockLogin).toHaveBeenCalledTimes(1);
+            expect(mockLogin).toHaveBeenCalledWith({
+                username: 'customer0',
+                password: 'verystrongpasword0',
+            });
+        });
+    });    
 });
