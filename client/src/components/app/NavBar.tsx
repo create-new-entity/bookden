d5f422fa74311d32e1febe916e4ad3c3653463cf
Pavel <imran.pavel@gmail.com>
2026-01-31 21:55:47 +0200

    Update NavBar.tsx

--- client/src/components/app/NavBar.tsx
+++ client/src/components/app/NavBar.tsx
@@ -98,6 +98,7 @@ const NavBar = () => {
                             setMenuOpen(false);
                             setAnchorElement(null);
                         }}
+                        disableScrollLock
                     >
                         {
                             options.map((option) => {
