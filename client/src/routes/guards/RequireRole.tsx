cc3aeb703ee990e3fdbee2cc7c2363c894bc2e27
Pavel <imran.pavel@gmail.com>
2026-02-03 15:41:20 +0200

    Update RequireRole.tsx

--- client/src/routes/guards/RequireRole.tsx
+++ client/src/routes/guards/RequireRole.tsx
@@ -1,6 +1,6 @@
 import { Navigate, Outlet } from 'react-router-dom';
 
-import { AUTH, UNAUTHORIZED } from '../../constants';
+import { UNAUTHORIZED } from '../../constants';
 import { useAuthContext } from '../../contexts';
 import type { UserType } from '../../types';
 
