85e0fac34de2e92e84f1d959fa269398570c924c
Pavel <imran.pavel@gmail.com>
2026-03-15 13:56:18 +0200

    order and order items tables migration

---
+++ server/migrations/sqls/20260315112312-orders-and-order-items-up.sql
@@ -0,0 +1,23 @@
+
+
+
+CREATE TABLE IF NOT EXISTS orders (
+    order_id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    total_price NUMERIC(10, 2) NOT NULL,
+
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS order_items (
+    order_item_id SERIAL PRIMARY KEY,
+    order_id INTEGER NOT NULL REFERENCES orders(order_id) ON DELETE CASCADE,
+    book_id INTEGER NOT NULL REFERENCES books(book_id) ON DELETE RESTRICT,
+    quantity INTEGER NOT NULL DEFAULT 1 CHECK (quantity > 0),
+    unit_price_snapshot NUMERIC(10, 2) NOT NULL,
+    title_snapshot TEXT NOT NULL,
+
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (order_id, book_id)   -- An order can not have same book twice. It is not the same as having quantity more than one.
+);
