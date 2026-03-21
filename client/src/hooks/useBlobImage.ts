29754bcc7be747b81c5b7bca92610e7fea89f31e
Pavel <imran.pavel@gmail.com>
2026-03-21 11:13:29 +0200

    Update useBlobImage.ts

--- client/src/hooks/useBlobImage.ts
+++ client/src/hooks/useBlobImage.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import type { AxiosError } from 'axios';
 import { useEffect, useMemo } from 'react';
 
@@ -32,6 +32,7 @@ export const useBlobImage = ({ queryKey, queryFn, enabled = true }: UseBlobImage
     const blobResult = useQuery<Blob, AxiosError>({
         queryKey, queryFn, enabled, retry: false
     });
+    const queryClient = useQueryClient();
 
     const objectUrl = useMemo(() => {
         const noValidBlobImage = blobResult.isError || !blobResult.data;
@@ -43,11 +44,22 @@ export const useBlobImage = ({ queryKey, queryFn, enabled = true }: UseBlobImage
 
     useEffect(() => {
         return () => {
-            if (objectUrl) {
+
+            /*
+                Note to future self:
+
+                Why check for isStillInUse?
+                Because we don't want to revoke the object url if the query is still in use.
+                If the query is still in use, the object url is still valid.
+                If we revoke early -> loads of warning logs in the console about "GET blob:... failed" -> refetch.
+            */
+            const state = queryClient.getQueryState(queryKey);
+            const isStillInUse = state?.data !== undefined;
+            if (!isStillInUse && objectUrl) {
                 URL.revokeObjectURL(objectUrl);
             }
         };
-    }, [objectUrl]);
+    }, [objectUrl, queryClient, queryKey]);
 
     return {
         objectUrl,
